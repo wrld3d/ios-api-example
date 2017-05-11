65b19d48c814f33d0fc1d7ccda4433b714636eb0
Calum Leask <calum.leask@eegeo.com>
2017-05-11 14:46:57 +0100

    Fixed the margins of the embedded map example
    Buddy: Blair

--- ApiSamples/Samples/BasicMap.m
+++ ApiSamples/Samples/BasicMap.m
@@ -17,12 +17,11 @@
     
     CGRect viewBounds = self.view.bounds;
     
-    CGFloat top = 96;
     CGFloat border = 32;
-    viewBounds.size.width -= 2*border;
-    viewBounds.size.height -= top + border;
+    viewBounds.size.width -= 2 * border;
+    viewBounds.size.height -= 2 * border;
     viewBounds.origin.x += border;
-    viewBounds.origin.y += top;
+    viewBounds.origin.y += border;
     
     _mapView = [[WRLDMapView alloc] initWithFrame:viewBounds];
     
