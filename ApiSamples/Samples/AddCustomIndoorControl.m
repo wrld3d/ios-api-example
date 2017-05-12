3c688d4f3e7f898a7b2d28d3dcfe53a81e88ab12
Paul Harris <paul.harris@eegeo.com>
2017-05-12 11:36:13 +0100

    Fix indoor control buttons to match Android sample
    Buttons now disable when outdoors, have titles that match Android, and fit on the screen better.
    
    Buddy: Vimmy

--- ApiSamples/Samples/AddCustomIndoorControl.m
+++ ApiSamples/Samples/AddCustomIndoorControl.m
@@ -3,9 +3,10 @@
 @import WrldWidgets;
 
 
-@interface AddCustomIndoorControl ()
+@interface AddCustomIndoorControl () <WRLDIndoorMapDelegate>
 
 @property (nonatomic) WRLDMapView *mapView;
+@property (nonatomic) NSArray<UIButton *> *buttons;
 
 @end
 
@@ -25,44 +26,76 @@
     
     [self.view addSubview:_mapView];
     
-    [self addButton: @"Exit Indoor Map" withIndex: 0 andSelector: @selector(exitIndoorMap)];
-    [self addButton: @"Go Up One Floor" withIndex: 1 andSelector: @selector(moveUpOneFloor)];
-    [self addButton: @"Go Down Floor" withIndex: 2 andSelector: @selector(moveDownOneFloor)];
-    [self addButton: @"Go To Lowest Floor" withIndex: 3 andSelector: @selector(goToLowestFloor)];
+    NSMutableArray<UIButton *>* buttons = [NSMutableArray array];
+    
+    [buttons addObject: [self addButton: @"TOP FLOOR" withIndex: 0 andSelector: @selector(topFloorButton)]];
+    [buttons addObject: [self addButton: @"MOVE UP" withIndex: 1 andSelector: @selector(moveUpButton)]];
+    [buttons addObject: [self addButton: @"MOVE DOWN" withIndex: 2 andSelector: @selector(moveDownButton)]];
+    [buttons addObject: [self addButton: @"BOTTOM FLOOR" withIndex: 3 andSelector: @selector(bottomFloorButton)]];
+    [buttons addObject: [self addButton: @"EXIT" withIndex: 4 andSelector: @selector(exitButton)]];
+    
+    _buttons = buttons;
+    
+    [_mapView setIndoorMapDelegate:self];
 }
 
-- (void) addButton: (NSString*)title withIndex: (NSInteger)index andSelector: (SEL)selector
+- (UIButton*) addButton: (NSString*)title withIndex: (NSInteger)index andSelector: (SEL)selector
 {
-    CGFloat height = 50.0;
+    CGFloat height = 40.0;
+    CGFloat spacing = 10.0;
     UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     [button addTarget:self
                    action:selector
          forControlEvents:UIControlEventTouchUpInside];
     
     [button setTitle:title forState:UIControlStateNormal];
-    [button setFrame:CGRectMake(0.0, 100.0 + height * index, 250.0, height)];
+    [button setFrame:CGRectMake(0.0, 44.0 + (height+spacing) * index, 125.0, height)];
     [button setBackgroundColor:[UIColor whiteColor]];
     [self.view addSubview:button];
+    [button setEnabled:NO];
+    return button;
 }
 
-- (void) exitIndoorMap
+- (void) exitButton
 {
     [_mapView exitIndoorMap];
 }
 
-- (void) goToLowestFloor
+- (void) topFloorButton
+{
+    NSInteger numberOfFloors = [[[_mapView activeIndoorMap] floors] count];
+    [_mapView setFloorByIndex:numberOfFloors-1];
+}
+
+- (void) bottomFloorButton
 {
     [_mapView setFloorByIndex:0];
 }
 
-- (void) moveUpOneFloor
+- (void) moveUpButton
 {
     [_mapView moveUpFloor];
 }
 
-- (void) moveDownOneFloor
+- (void) moveDownButton
 {
     [_mapView moveDownFloor];
+}
+
+- (void) didEnterIndoorMap
+{
+    for (UIButton *button in _buttons)
+    {
+        [button setEnabled:YES];
+    }
+}
+
+- (void) didExitIndoorMap
+{
+    for (UIButton *button in _buttons)
+    {
+        [button setEnabled:NO];
+    }
 }
 
 @end
