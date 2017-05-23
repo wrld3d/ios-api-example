0e1d434ee1ed8512c9404d41796ebb9d227a1a1b
jonty-dawson <jonty.dawson@eegeo.com>
2017-05-23 09:18:45 +0100

    implement screen pick sample bump WRLD version for new api point Buddy: Judith

--- ApiSamples/Samples/PickScreenPoint.m
+++ ApiSamples/Samples/PickScreenPoint.m
@@ -3,19 +3,23 @@
 @import Wrld;
 
 
-@interface PickScreenPoint ()
+@interface PickScreenPoint () <WRLDMapViewDelegate>
 
 @property (nonatomic) WRLDMapView *mapView;
 
 @end
 
 @implementation PickScreenPoint
+{
+    WRLDMarker* marker;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
     _mapView = [[WRLDMapView alloc] initWithFrame:self.view.bounds];
+    _mapView.delegate = self;
     
     _mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     
@@ -26,6 +30,25 @@
     [self.view addSubview:_mapView];
 }
 
+#pragma mark - WRLDMapViewDelegate implementation
+
+- (void)mapView:(WRLDMapView *)mapView didTapMap:(WRLDCoordinateWithAltitude)coordinateWithAltitude
+{
+    NSString *message = [NSString stringWithFormat:@"LatLng [%f, %f]; altitude %f m", coordinateWithAltitude.coordinate.latitude, coordinateWithAltitude.coordinate.longitude, coordinateWithAltitude.altitude];
+    [SamplesMessage showWithMessage:message
+                        andDuration:[NSNumber numberWithInt:3]];
+    
+    if (marker != nil)
+    {
+        [_mapView removeMarker:marker];
+        marker = nil;
+    }
+    
+    marker = [WRLDMarker markerAtCoordinate:coordinateWithAltitude.coordinate];
+    marker.elevationMode = HeightAboveSeaLevel;
+    marker.elevation = coordinateWithAltitude.altitude;
+    [_mapView addMarker:marker];
+}
 
 
 
