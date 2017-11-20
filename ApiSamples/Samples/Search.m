71ffbcdaf5dbc9532afb886570f411754cdd3c2c
Tom Paynter <tom.paynter@wrld3d.com>
2017-11-20 14:52:26 +0000

    Rename and refactor of search widget. Buddy: sam.ainsworth

--- ApiSamples/Samples/Search.m
+++ ApiSamples/Samples/Search.m
@@ -4,16 +4,10 @@
 @import Wrld;
 @import WrldWidgets;
 
-
 @implementation Search
 {
-    WRLDSearchWidgetView *m_tableView;
-    
-    WRLDSearchModule* m_searchModule;
-    
-    WRLDPoiService* m_poiService;
-    
-    POIServiceSuggestionProvider* m_poiSearchProvider;
+    WRLDSearchWidgetView *m_searchWidgetView;
+    WRLDSearchModule *m_searchModule;
 }
 
 - (void)viewDidLoad
@@ -21,37 +15,20 @@
     [super viewDidLoad];
     
     WRLDMapView *mapView = [[WRLDMapView alloc] initWithFrame:self.view.bounds];
-    
     mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    
-    // set the center of the map and the zoom level
     [mapView setCenterCoordinate:CLLocationCoordinate2DMake(37.7858, -122.401)
                        zoomLevel:15
                         animated:NO];
-    
     [self.view addSubview:mapView];
     
-    
-    m_tableView = [[WRLDSearchWidgetView alloc ] initWithFrame:CGRectMake(10, 10, 300, 500)];
-    [self.view addSubview:m_tableView];
-    
-    
     m_searchModule = [[WRLDSearchModule alloc] init];
+    [m_searchModule addSearchProvider: [[POIServiceSearchProvider alloc] initWithMapViewAndPoiService: mapView poiService: [mapView createPoiService]]];
     
+    m_searchWidgetView = [[WRLDSearchWidgetView alloc ] initWithFrame:CGRectMake(10, 10, 300, 500)];
+    [m_searchWidgetView setSearchModule:m_searchModule];
+    [self.view addSubview:m_searchWidgetView];
     
-    
-    m_poiService = [mapView createPoiService];
-    
-    m_poiSearchProvider = [[POIServiceSuggestionProvider alloc] initWithMapViewAndPoiService: mapView poiService: m_poiService];
-    
-    
-    [m_searchModule addSuggestionProvider: m_poiSearchProvider];
-    
-    [m_tableView setSearchModule:m_searchModule];
-    
-    
-    [m_searchModule doAutoCompleteQuery: @"Yerba"];
-    
+    [m_searchModule search:@"Mouse"];
 }
 
 @end
