e8259affb25141af944b1920d2cad77c39f516f8
Michael O'Donnell <michael.odonnell@wrld3d.com>
2018-02-07 11:52:38 +0000

    Restart for production sample

--- ApiSamples/Samples/SearchWidget.m
+++ ApiSamples/Samples/SearchWidget.m
@@ -5,10 +5,6 @@
 @import WrldWidgets;
 
 @implementation SearchWidget
-{
-    WRLDSearchWidgetView *m_searchWidgetView;
-    POIServiceSearchProvider *wrldPoiSearchProvider;
-}
 
 - (void)viewDidLoad
 {
@@ -21,33 +17,26 @@
                         animated:NO];
     [self.view addSubview:mapView];
     
-    wrldPoiSearchProvider = [[POIServiceSearchProvider alloc] initWithMapViewAndPoiService: mapView poiService: [mapView createPoiService]];
     
     CGRect searchFrame = ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) ?
         CGRectMake(10, 10, 375, 568) :
         CGRectMake((self.view.bounds.size.width-375)/2.0f, 10, 375, 568);
     
-    if(@available(iOS 10.0, *))
-    {
-        m_searchWidgetView = [[WRLDSearchWidgetViewWithSpeechRecognition alloc ] initWithFrame:searchFrame];
-    }
-    else{
-        m_searchWidgetView = [[WRLDSearchWidgetView alloc] initWithFrame:searchFrame];
-    }
+    WRLDSearchWidgetViewController* searchWidgetViewController = [[WRLDSearchWidgetViewController alloc] initWithFrame:searchFrame];
     
-    //[m_searchWidgetView setSearchModule:m_searchWidgetViewController];
-    [m_searchWidgetView addSearchProvider:wrldPoiSearchProvider];
+//    POIServiceSearchProvider * wrldPoiSearchProvider = [[POIServiceSearchProvider alloc] initWithMapViewAndPoiService: mapView poiService: [mapView createPoiService]];
+//    [m_searchWidgetViewController addSearchProvider:wrldPoiSearchProvider];
+//
+//    MockSearchProvider* mockProvider = [[MockSearchProvider alloc] init];
+//    [m_searchWidgetViewController addSearchProvider:mockProvider];
+//
+//    NSBundle* widgetsBundle = [NSBundle bundleForClass:[WRLDSearchWidgetView class]];
+//
+//    UINib* yelpNib = [UINib nibWithNibName:@"YelpSearchResultCell" bundle:widgetsBundle];
+//
+//    [m_searchWidgetViewController registerCellForResultsTable:@"YelpSearchResultCell" :yelpNib];
     
-    MockSearchProvider* mockProvider = [[MockSearchProvider alloc] init];
-    [m_searchWidgetView addSearchProvider:mockProvider];
-    
-    NSBundle* widgetsBundle = [NSBundle bundleForClass:[WRLDSearchWidgetView class]];
-    
-    UINib* yelpNib = [UINib nibWithNibName:@"YelpSearchResultCell" bundle:widgetsBundle];
-    
-    [m_searchWidgetView registerCellForResultsTable:@"YelpSearchResultCell" :yelpNib];
-    
-    [self.view addSubview:m_searchWidgetView];
+    [self.view addSubview: searchWidgetViewController.rootView];
 }
 
 @end
