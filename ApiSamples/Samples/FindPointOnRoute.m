cfcdcc92e5e8fd864e7dbb09d9770a7d5bc16551
MichaelChanEeGeo <michael.chan@kimbleoperations.com>
2018-05-28 10:59:44 +0100

    Removing route markers from FindPointOnRotue Buddy: Jordan

--- ApiSamples/Samples/FindPointOnRoute.m
+++ ApiSamples/Samples/FindPointOnRoute.m
@@ -60,19 +60,6 @@
             WRLDRouteView* routeView = [[WRLDRouteView alloc] initWithMapView:mapView route:route options:options];
             [_routeViews addObject:(routeView)];
             
-            for (WRLDRouteSection* section in route.sections)
-            {
-                for (WRLDRouteStep* step in section.steps)
-                {
-                    WRLDMarker* marker = [WRLDMarker markerAtCoordinate:*(step.path)];
-                    if (step.isIndoors)
-                    {
-                        marker = [WRLDMarker markerAtCoordinate:*(step.path) inIndoorMap:step.indoorId onFloor:step.indoorFloorId];
-                    }
-                    [mapView addMarker:marker];
-                }
-            }
-            
             WRLDPointOnRouteOptions* pointOnRouteOptions = [[WRLDPointOnRouteOptions alloc] init];
             WRLDPointOnRouteResult* pointOnRouteInfo = [_pointOnPath getPointOnRoute:route point:testCoordinate options:pointOnRouteOptions];
             
